917b5d5107e38a90dd5d2598eab72f19d9d5baf2
HsOjo <1134031392@qq.com>
2020-11-09 12:11:06 +0800

    Add vant.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import './plugins/axios'
 import App from './App.vue'
 import store from './store'
 import router from './router'
+import './plugins/vant.js'
 
 Vue.config.productionTip = false
 
---
+++ src/plugins/vant.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+// eslint-disable-next-line no-unused-vars
+import Vant, { Locale } from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
